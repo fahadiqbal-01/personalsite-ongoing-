91f143030a877d88053baaf5e5f94c049282bbe8
FahadIqbal-01 <fahad.iqbal.im.47@gmail.com>
2024-04-10 03:23:15 +0600

    extra small device responsive (on-going)

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ let p_content = document.querySelector(".portfolio");
 
 let profile_img = document.querySelector("#profile_image");
 let profile = document.querySelector(".profile");
+let allcontent = document.querySelector(".all_details");
 
+let navbtn = document.querySelector(".nav_button");
+let navlist = document.querySelector(".nav_list");
 
 hbtn.addEventListener("click", () => {
   h_content.style.display = "block";
@@ -46,4 +49,40 @@ pbtn.addEventListener("click", () => {
   r_content.style.display = "none";
   profile_img.style.display = "none";
   profile.style.height = "440px";
+});
+
+hbtn.addEventListener("click", () => {
+  if (screen.width <= 640) {
+    profile.style.display = "block";
+  } else {
+    profile.style.display = "block";
+  }
+});
+
+abtn.addEventListener("click", () => {
+  if (screen.width <= 640) {
+    profile.style.display = "none";
+  } else {
+    profile.style.display = "block";
+  }
+});
+
+rbtn.addEventListener("click", () => {
+  if (screen.width <= 640) {
+    profile.style.display = "none";
+  } else {
+    profile.style.display = "block";
+  }
+});
+
+pbtn.addEventListener("click", () => {
+  if (screen.width <= 640) {
+    profile.style.display = "none";
+  } else {
+    profile.style.display = "block";
+  }
+});
+
+navbtn.addEventListener("click", ()=>{
+  navlist.style.display = "block"
 });
